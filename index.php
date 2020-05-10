b87049923eadf2f3fea2359afc0b034e93042f9b
Oswald Quevillart <o.quevillart@codeur.online>
2020-05-10 17:08:32 +0200

    update

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+include "header.php";
+
+ ?>
+
+
+<header>
+
+  <nav class="navbar navbar-expand-lg mb-10 mx-auto">
+  <a class="navbar-brand flex-grow-1" href="#"> <img src="img/logo.png" alt="logo"> </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link text-white" href="index.php">Subscribe<span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link active text-white bg-dark" href="index.php">Get It Now</a>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+  <div class="row mx-auto mb-0">
+    <div class="col1"><img src="img/phone.png" alt="phone">
+    </div>
+    <div class="col2">
+      <h1 class="headertitle pb-5">Join more than <span class="text-white">1000+</span> <br> satisfied users</h1>
+      <div class="headertext py-5 text-white">
+      <a>People are definitely a compagny's greatest asset.<br>It dosen't make any difference<br> wheter the product is cars or cosmetics.<br>
+      A compagny is only as good as the people it keeps.</a><br>
+      </div>
+      <a href="index.php" />
+      <img class="androidbutton py-5"src="img/androidappbutton.png" alt="android application download"/>
+      </div>
+  </div>
+</div>
+
+</header>
+
+<section>
+  <div class="container">
+    <div class="row mx-auto mb-0">
+      <div class="headertext py-5 text-white">
+        <h1 class="headertitle pb-5 text-white">What you Get</h1>
+      <a>People are definitely a compagny's greatest asset.<br>It dosen't make any difference<br> wheter the product is cars or cosmetics.<br>
+      A compagny is only as good as the people it keeps.</a><br>
+      <button class="btn" type="getit">Get It Now</button>
+      </div>
+      </div>
+      <div class="col2">
+        <a href="index.php" />
+        </div>
+    </div>
+  </div>
+</section>
+
+
+ <?php
+
+ include "footer.php";
---
+++ indexdupli.php
@@ -0,0 +1,29 @@
+<?php
+
+include "header.php";
+
+ ?>
+
+
+<header>
+
+  <nav class="navbar navbar-expand-lg mx-auto">
+  <a class="navbar-brand flex-grow-1" href="#"> <img src="img/logo.png" alt="logo"> </a>
+  <button class="navbar-toggler" type="submit" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link text-white" href="index.php">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link text-white" href="index.php">Features</a>
+    </div>
+  </div>
+</nav>
+
+</header>
+
+
+
+ <?php
+
+ include "footer.php";
