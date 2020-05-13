f3bedb0f0855da5c5ca77069806aef1c86ad24a5
Oswald Quevillart <o.quevillart@codeur.online>
2020-05-13 16:49:13 +0200

    update navbar

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include "header.php";
 <header>
   <nav class="navbar navbar-expand-lg navbar-light mb-10 mx-auto">
   <a class="navbar-brand flex-grow-1" href="#"> <img src="img/logo.png" alt="logo"> </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
